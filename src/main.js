9a892a2596ab8394ee5f83fa726759ac72f6d277
unknown <gregmasterbr@gmail.com>
2023-06-04 20:22:13 -0300

    Implementado função para remover item da lista e da DOM. Foi adicionado na lista um ID para cada tarefa. Esse ID é o timestamp gerado no momento de criar a tarefa. Ainda não está 100% funcional.

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ window.addEventListener("load", (event)=>{
     const todoList = [];
 	const todoContainer = document.querySelector("#todoContainer");	
     const bttAddTodo = document.querySelector('#bttAddTodo');
-	
+	//verificaSeListaEstaVazia();
     bttAddTodo.addEventListener('click',(e)=>{
         new_task = document.querySelector('#newTask');
         if (!new_task || new_task.value.length === 0){
@@ -12,45 +12,63 @@ window.addEventListener("load", (event)=>{
         }
         todo = addTodoList(new_task.value);
 		addTaskInTemplate(todo);
+        new_task.value="";
+        //verificaSeListaEstaVazia();
+        new_task.focus();
     });
 
 	function addTaskInTemplate(todo){
 		var value =`
-			<input type="checkbox" id="task1" class="">
-			<label class="form-check-label" for="task1">${todo.tarefa}</label>
+			<input type="checkbox" id="${todo.id}_ckd" class="task ">
+			<label class="form-check-label"  style="min-width:100px;"for="${todo.id}_ckd">${todo.tarefa}</label>
+            <button type="button" class="btn-close removerTarefa" aria-label="Close" onclick="excluirTarefa(${todo.id});"></button>
 		`
 		var li = document.createElement("li");
-		li.className = "list-group-item"
+		li.className = "list-group-item";
+        li.id= todo.id;
 		li.innerHTML = value;
 		todoContainer.appendChild(li);
     }
 
 	function addTodoList(item){		
 		const todo = {
+            id: Date.now(),
 			tarefa: item,
 			done: false,
 		}
-		todoList.push(todo);
-		
+		todoList.push(todo);		
 		console.log(todoList);
 		return todo;
     }
+
+
+    function excluirTarefa(id){
+        var index = -1
+        for (i=0; i< todoList.length; i++){
+            el = todoList[i];
+            if (el.id == id){
+                console.log(el);
+                //delete arr[i];
+                index = i;               
+            }
+        }        
+        if (index > -1) {
+            todoList.splice(index, 1);
+            //document.getElementById(el.id);
+            //todoContainer.removeChild(document.getElementById(el.id));
+        }        
+    }
+
+    function verificaSeListaEstaVazia(){
+        const itensTasks = todoContainer.querySelectorAll("li");
+      
+        if (itensTasks.length == 0) {
+            alerta= `<div class="alert alert-warning" role="alert">
+            Não existe nenhuma tarefa adicionada na lista!
+            </div>`;            
+            todoContainer.innerHTML = alerta;	
+        }else{
+            todoContainer.parentElement.remove($('.alert'));
+        }        
+    }    
 });
-
-/*
-const tag_li =  document.createElement("li");
-tag_li.setAttribute('class', 'list-group-item');
-
-const tag_input_checkbox =  document.createElement("input");
-tag_input_checkbox.setAttribute('type', 'checkbox');
-tag_input_checkbox.setAttribute('class', 'form-check-input');
-tag_input_checkbox.setAttribute('onclick', ()=> handleCheckBox(this));
-tag_input_checkbox.setAttribute('value', todo.tarefa);
-
-
-const tag_label =  document.createElement("label");
-tag_label.setAttribute('class', 'form-check-label');
-tag_label.innerHTML = todo.tarefa;
-
-*/
-
